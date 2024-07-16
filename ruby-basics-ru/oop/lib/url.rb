7e07e03e55d4295b61795c6ad04944aff050eae6
@hexlet/cli <support@hexlet.io>
2024-07-16 23:18:12 +0300

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# BEGIN
+class Url
+  include Comparable
+
+  attr_accessor :scheme, :host, :port
+
+  def initialize(address)
+    uri = URI(address)
+
+    @scheme = uri.scheme
+    @host = uri.host
+    @port = uri.port
+    @path = uri.path
+
+    uri_query = uri.query
+    decoded_query = uri_query.nil? ? [] : URI.decode_www_form(uri_query)
+    @query = decoded_query.to_h.transform_keys &:to_sym
+  end
+
+  def query_params
+    @query
+  end
+
+  def query_param(key, value = nil)
+    @query[key] || value
+  end
+
+  def <=>(other)
+    precedence <=> other.precedence
+  end
+
+  protected
+
+  def precedence
+    [scheme, host, port, query_params]
+  end
+end
+# END
